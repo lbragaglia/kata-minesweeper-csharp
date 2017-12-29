24a759d50b22324bf2c8a2eb101aa673d0dd4778
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2017-12-29 19:43:37 +0100

    Simple jagged array solution

--- KataMinesweeperTests.cs
+++ KataMinesweeperTests.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace KataMinesweeper
@@ -18,40 +21,76 @@ namespace KataMinesweeper
         [InlineData("*.*", "*2*")]
         [InlineData("*..*", "*11*")]
         [InlineData("*...*", "*101*")]
-        public void Test1(string minesField, string adjacencyField)
+        public void Test1D(string minesField, string adjacencyField)
         {
-            var game = new Game(minesField);
+            var game = new Game(1, minesField.Length, minesField);
+            Assert.Equal(adjacencyField, game.AdjacencyField());
+        }
+
+        [Theory]
+        [InlineData(2, 2, "....", "0000")]
+        [InlineData(2, 2, "...*", "111*")]
+        [InlineData(3, 3, "....*....", "1111*1111")]
+        [InlineData(5, 5, "............*............", "000000111001*100111000000")]
+        [InlineData(4, 4, "*........*......", "*10022101*101110")]
+        [InlineData(3, 5, "**.........*...", "**100332001*100")]
+        public void Test2D(int lines, int columns, string minesField, string adjacencyField)
+        {
+            var game = new Game(lines, columns, minesField);
             Assert.Equal(adjacencyField, game.AdjacencyField());
         }
     }
     public class Game
     {
-        private readonly string _minesField;
-        private readonly int _fieldSize;
-        private readonly int _extFieldSize;
-        private const int BorderSize = 2;
+        private readonly string[] _minesField;
 
-        public Game(string initialField)
+        public Game(int lines, int columns, string initialField)
         {
-            _minesField = initialField;
-            _fieldSize = initialField.Length;
-            _extFieldSize = _fieldSize + BorderSize;
+            var emptyLine = new[] { string.Join("", Enumerable.Repeat('.', columns + 2)) };
+            var splittedLines = SplitLines(initialField, columns);
+            _minesField = emptyLine
+                .Concat(splittedLines)
+                .Concat(emptyLine)
+                .ToArray();
+        }
+
+        private static IEnumerable<string> SplitLines(string field, int lineSize)
+        {
+            for (int i = 0; i < field.Length; i += lineSize)
+            {
+                yield return '.' + field.Substring(i, Math.Min(lineSize, field.Length - i)) + '.';
+            }
         }
 
         internal string AdjacencyField()
         {
-            var extMinesField = "." + _minesField + ".";
-            var adjacencyField = new int[_extFieldSize];
-            for (int i = 1; i < extMinesField.Length - 1; i++)
+            var adjacencyField = new int[_minesField.Length][];
+
+            for (int i = 0; i < _minesField.Length; i++)
             {
-                if (extMinesField[i] == '*')
-                {
-                    adjacencyField[i - 1] ++;
-                    adjacencyField[i + 1] ++;
-                }
+                adjacencyField[i] = new int[_minesField[i].Length];
             }
 
-            return string.Join("", adjacencyField.Zip(extMinesField, CellToString).Skip(1).Take(_minesField.Length));
+            for (int i = 1; i < _minesField.Length - 1; i++)
+            {
+                for (int j = 1; j < _minesField[i].Length - 1; j++)
+                {
+                    if (_minesField[i][j] == '*')
+                    {
+                        adjacencyField[i - 1][j - 1] ++;
+                        adjacencyField[i - 1][j + 0] ++;
+                        adjacencyField[i - 1][j + 1] ++;
+                        adjacencyField[i + 0][j - 1] ++;
+                        adjacencyField[i + 0][j + 1] ++;
+                        adjacencyField[i + 1][j - 1] ++;
+                        adjacencyField[i + 1][j + 0] ++;
+                        adjacencyField[i + 1][j + 1] ++;
+                    }
+                }
+            }
+            
+            return string.Join("", adjacencyField.Skip(1).Take(_minesField.Length - 2)
+                .SelectMany((line, i) => line.Zip(_minesField[i + 1], CellToString).Skip(1).Take(_minesField[i + 1].Length - 2)));
         }
 
         private string CellToString(int adjacentMines, char originalCell) => originalCell == '*' ? "*" : adjacentMines.ToString();
